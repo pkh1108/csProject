7a0eb2bcf75bdf7ab54848c0c0aeff55557a4fc4
pkh1108 <zzrkguszz@naver.com>
2018-05-14 14:15:35 +0900

    오프닝 제목, 플레이 글씨 구현, 플레이 버튼 위치 수정해야함 미완

---
+++ Dark_Echo/Dark_Echo/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Dark_Echo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.BackColor = System.Drawing.SystemColors.Window;
+            this.PlayButton.Location = new System.Drawing.Point(527, 718);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.UseVisualStyleBackColor = false;
+            this.PlayButton.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.PlayButton);
+            this.Name = "MainForm";
+            this.Text = "Dark_Echo";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+    }
+}
+
---
+++ Dark_Echo/Dark_Echo/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dark_Echo
+{
+    public partial class MainForm : Form
+    {
+        Size FormSize;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            InitializeComponent();
+            FormSize.Width = MainForm.ActiveForm.Size.Width;
+            FormSize.Height = MainForm.ActiveForm.Size.Height;
+
+            PrivateFontCollection privateFonts = new PrivateFontCollection();
+            Graphics g = CreateGraphics();
+
+            // 오프닝 제목
+            privateFonts.AddFontFile("AmaticSC-Bold.ttf");
+            Font f = new Font(privateFonts.Families[0], 100);
+            g.DrawString("Dark  Echo", f, Brushes.White, FormSize.Width / 4, FormSize.Height / 8);
+            f.Dispose();
+
+            // 플레이 버튼
+            string s = "P L A Y";
+            f = new Font(privateFonts.Families[0], 50);
+            SizeF sf = g.MeasureString(s, f);
+            g.DrawString(s, f, Brushes.White, FormSize.Width / 5 * 2, FormSize.Height / 2);
+            g.DrawRectangle(Pens.White, FormSize.Width / 5 * 2, FormSize.Height / 2, sf.Width, sf.Height);
+            f.Dispose();
+
+            privateFonts.Dispose();
+            
+        }
+
+        
+    }
+}
